681f4735b9f31cc7e0b79725f9531baab94de064
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-21 22:58:23 +0300

    add controller of Auth

---
+++ QesatlyAPI/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Qesatly.Service.Abstracts;
+using Qesatly.Service.DTO;
+
+namespace QesatlyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            var response = await _authService.LoginAsync(loginDto);
+            return StatusCode((int)response.StatusCode, response);
+        }
+    }
+}
