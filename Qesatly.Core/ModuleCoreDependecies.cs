b26d181fc8c58bd8b9c9bd1a5f7abbc89fa5f024
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-21 22:57:45 +0300

    add dependency of core

---
+++ Qesatly.Core/ModuleCoreDependecies.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Qesatly.Core.Bases;
+
+namespace Qesatly.Core
+{
+    public static class ModuleCoreDependecies
+    {
+        public static IServiceCollection AddCoreDependecies(this IServiceCollection services)
+        {
+            services.AddTransient<ResponseHandler>();
+            return services;
+        }
+    }
+}
