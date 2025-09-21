1c99adb0035ede3bb6e6859be3d668fd1d09dab7
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-21 22:58:52 +0300

    add Dependencies

--- QesatlyAPI/Program.cs
+++ QesatlyAPI/Program.cs
@@ -1,4 +1,7 @@
-
+using Microsoft.OpenApi.Models;
+using Qesatly.Core;
+using Qesatly.Service;
+using Swashbuckle.AspNetCore.SwaggerGen;
 namespace QesatlyAPI
 {
     public class Program
@@ -10,9 +13,60 @@ namespace QesatlyAPI
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("MyPolicy", builder =>
+                {
+                    builder
+                        .SetIsOriginAllowed(_ => true)
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials();
+                });
+            });
+
+            #region Dependency Injection
+
+            builder.Services.AddServiceDependencies()
+                .AddCoreDependecies();
+            #endregion
+
+            #region SwaggerRegion
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Skilly API", Version = "v1" });
+                c.CustomSchemaIds(type => type.FullName);
+                c.SchemaGeneratorOptions = new SchemaGeneratorOptions
+                {
+                    UseAllOfToExtendReferenceSchemas = false
+                };
+                // Add support for bearer token authentication
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter 'Bearer' followed by a space and the token.",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+                { new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+            new string[] {}
+        }
+                });
+            });
+
+            #endregion
 
             var app = builder.Build();
 
@@ -25,8 +79,10 @@ namespace QesatlyAPI
 
             app.UseHttpsRedirection();
 
+            app.UseRouting();
+            app.UseCors("MyPolicy");
+            app.UseAuthentication();
             app.UseAuthorization();
-
 
             app.MapControllers();
 
