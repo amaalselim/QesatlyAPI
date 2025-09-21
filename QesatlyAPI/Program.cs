656e3919f8c31baaffb4a036a33371db3e3153c5
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-21 23:13:17 +0300

    allow publishing

--- QesatlyAPI/Program.cs
+++ QesatlyAPI/Program.cs
@@ -71,11 +71,8 @@ namespace QesatlyAPI
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
