05742ba9238a0398d63a78395ea48f6c6ad45a51
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-21 22:57:53 +0300

    interface of auth

---
+++ Qesatly.Service/Abstracts/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Qesatly.Core.Bases;
+using Qesatly.Service.DTO;
+
+namespace Qesatly.Service.Abstracts
+{
+    public interface IAuthService
+    {
+        Task<Response<string>> LoginAsync(LoginDto loginDto);
+
+    }
+}
