9ff875fe795213f9db108331a7c64f60e7516ca7
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-23 02:08:32 +0300

    add migration

---
+++ Qesatly.Infrastructure/Migrations/20250922230605_add-foreignkey.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Qesatly.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addforeignkey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ClientsId",
+                table: "products",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "clientId",
+                table: "products",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_products_ClientsId",
+                table: "products",
+                column: "ClientsId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_products_clients_ClientsId",
+                table: "products",
+                column: "ClientsId",
+                principalTable: "clients",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_products_clients_ClientsId",
+                table: "products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_products_ClientsId",
+                table: "products");
+
+            migrationBuilder.DropColumn(
+                name: "ClientsId",
+                table: "products");
+
+            migrationBuilder.DropColumn(
+                name: "clientId",
+                table: "products");
+        }
+    }
+}
