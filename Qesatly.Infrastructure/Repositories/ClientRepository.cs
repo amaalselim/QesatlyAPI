31d82079fd7749f271ec9e9dfd054a42e7c13326
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-23 02:01:16 +0300

    implement interface of ClientRepo

---
+++ Qesatly.Infrastructure/Repositories/ClientRepository.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using Qesatly.Core.Bases;
+using Qesatly.Data.Entities;
+using Qesatly.Infrastructure.Abstracts;
+using Qesatly.Infrastructure.Context;
+using Qesatly.Service.Abstracts;
+using Qesatly.Service.DTO;
+
+namespace Qesatly.Infrastructure.Repositories
+{
+    public class ClientRepository : IClientRepository
+    {
+        private readonly ApplicationDBContext _context;
+        private readonly IMapper _mapper;
+        private readonly IImageService _imageService;
+        private readonly ResponseHandler _responseHandler;
+
+        public ClientRepository(ApplicationDBContext context, IMapper mapper, IImageService imageservice,
+            ResponseHandler responseHandler)
+        {
+            _context = context;
+            _mapper = mapper;
+            _imageService = imageservice;
+            _responseHandler = responseHandler;
+        }
+
+        public async Task<Response<string>> AddAsync(AddClientDto client)
+        {
+            var clientmapper = _mapper.Map<Clients>(client);
+
+            if (client.Cardphoto != null)
+            {
+                var path = @"Images/Clients/Card/";
+                clientmapper.Cardphoto = await _imageService.SaveFileAsync(client.Cardphoto, path);
+            }
+            if (client.Isguarantor)
+            {
+                var path = @"Images/Clients/guarantorCard/";
+                if (client.guarantorCardphoto != null)
+                {
+                    clientmapper.guarantorCardphoto = await _imageService.SaveFileAsync(client.guarantorCardphoto, path);
+                }
+            }
+            await _context.clients.AddAsync(clientmapper);
+            await _context.SaveChangesAsync();
+            return _responseHandler.Created<string>("Client added successfully", $"id:{clientmapper.Id}");
+
+        }
+    }
+}
