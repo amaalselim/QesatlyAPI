a07d2e898a34679d038e49482f4faeea9716b59e
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-23 02:01:56 +0300

    auto mapper

---
+++ Qesatly.Service/Mapping/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Qesatly.Data.Entities;
+using Qesatly.Service.DTO;
+
+namespace Qesatly.Service.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<AddClientDto, Clients>()
+                .ForMember(dest => dest.Cardphoto, opt => opt.Ignore())
+                .ForMember(dest => dest.guarantorCardphoto, opt => opt.Ignore());
+
+        }
+    }
+}
