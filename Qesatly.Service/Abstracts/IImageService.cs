a43f1d6ce6a7104b6a05bb6b4cab264ff4c7d29a
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-23 02:01:40 +0300

    create interface of IImageService

---
+++ Qesatly.Service/Abstracts/IImageService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Qesatly.Service.Abstracts
+{
+    public interface IImageService
+    {
+        Task<string> SaveFileAsync(IFormFile Img, string folderPath);
+        Task DeleteFileAsync(string File);
+    }
+}
