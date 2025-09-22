26141b29f9b00c13dff9801ba2645f2179badff8
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-22 20:05:55 +0300

    add applicationDbContext

---
+++ Qesatly.Infrastructure/Context/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Qesatly.Infrastructure.Context
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext()
+        {
+
+        }
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+    }
+}
