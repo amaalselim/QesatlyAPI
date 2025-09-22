52033a264ef372cd424c8b12d77861d45e7cabe9
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-23 02:00:45 +0300

    create interface of ClientRepo

---
+++ Qesatly.Infrastructure/Abstracts/IClientRepository.cs
@@ -0,0 +1,10 @@
+﻿using Qesatly.Core.Bases;
+using Qesatly.Service.DTO;
+
+namespace Qesatly.Infrastructure.Abstracts
+{
+    public interface IClientRepository
+    {
+        Task<Response<string>> AddAsync(AddClientDto client);
+    }
+}
