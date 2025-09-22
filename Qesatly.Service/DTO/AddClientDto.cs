26a9c96dcf33ab9bb7020f151ff4f3d6131e1a01
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-23 02:01:04 +0300

    add clientdto

---
+++ Qesatly.Service/DTO/AddClientDto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Qesatly.Service.DTO
+{
+    public class AddClientDto
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public int NationalNumber { get; set; }
+        public string Address { get; set; }
+        public IFormFile Cardphoto { get; set; }
+        public bool Isguarantor { get; set; } = false;
+        public string? guarantorName { get; set; }
+        public string? guarantorPhone { get; set; }
+        public int? guarantorNationalNumber { get; set; }
+        public string? guarantorAddress { get; set; }
+        public IFormFile? guarantorCardphoto { get; set; }
+    }
+}
