db21696f98d87c2e9169757908e44be87af8be92
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-23 03:01:01 +0300

    add productDto

---
+++ Qesatly.Service/DTO/AddProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Qesatly.Service.DTO
+{
+    public class AddProductDto
+    {
+        public int clientId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public decimal DownPayment { get; set; }
+        public decimal insertRate { get; set; }
+        public int InstallmentCount { get; set; }
+    }
+}
