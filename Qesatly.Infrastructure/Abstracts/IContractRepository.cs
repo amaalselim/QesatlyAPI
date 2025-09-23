a5f4482043a2a6aaf6cc549097ea944f21b13c49
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-23 03:00:29 +0300

    create interface of Contract

---
+++ Qesatly.Infrastructure/Abstracts/IContractRepository.cs
@@ -0,0 +1,10 @@
+﻿using Qesatly.Core.Bases;
+using Qesatly.Service.DTO;
+
+namespace Qesatly.Infrastructure.Abstracts
+{
+    public interface IContractRepository
+    {
+        Task<Response<string>> AddAsync(AddProductDto product);
+    }
+}
