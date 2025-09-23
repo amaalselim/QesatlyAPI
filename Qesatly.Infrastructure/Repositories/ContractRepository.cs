7270d96ccd0bdb1a6e90c2174c5941e6962d4951
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-23 03:00:38 +0300

    implement interface of Contract

---
+++ Qesatly.Infrastructure/Repositories/ContractRepository.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using Qesatly.Core.Bases;
+using Qesatly.Data.Entities;
+using Qesatly.Infrastructure.Abstracts;
+using Qesatly.Infrastructure.Context;
+using Qesatly.Service.DTO;
+
+namespace Qesatly.Infrastructure.Repositories
+{
+    public class ContractRepository : IContractRepository
+    {
+        private readonly ApplicationDBContext _context;
+        private readonly IMapper _mapper;
+        private readonly ResponseHandler _responseHandler;
+
+        public ContractRepository(ApplicationDBContext context, IMapper mapper, ResponseHandler responseHandler)
+        {
+            _context = context;
+            _mapper = mapper;
+            _responseHandler = responseHandler;
+        }
+        public async Task<Response<string>> AddAsync(AddProductDto product)
+        {
+            var productmapper = _mapper.Map<Products>(product);
+            productmapper.clientId = product.clientId;
+            await _context.products.AddAsync(productmapper);
+            await _context.SaveChangesAsync();
+
+            decimal remaining = productmapper.Price - product.DownPayment;
+            decimal rate = product.insertRate / 100m;
+            decimal afterInterest = remaining * (1 + rate);
+            decimal installmentValue = afterInterest / product.InstallmentCount;
+            DateTime startDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1)
+                        .AddMonths(1);
+            Contracts contracts = new Contracts
+            {
+                productId = productmapper.Id,
+                clientId = product.clientId,
+                DownPayment = product.DownPayment,
+                insertRate = product.insertRate,
+                InstallmentCount = product.InstallmentCount,
+                StartDate = startDate,
+                EndDate = startDate.AddMonths(product.InstallmentCount),
+                CreatedAt = DateTime.Now.Date,
+                TotalPrice = afterInterest,
+                InstallmentValue = installmentValue
+            };
+
+            await _context.contracts.AddAsync(contracts);
+            await _context.SaveChangesAsync();
+            return _responseHandler.Created<string>("Product added successfully", null);
+        }
+    }
+}
