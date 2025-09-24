0a21ec4813631ad82f90b37231f273ae5b31b89d
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-24 22:39:06 +0300

    create enums of contracts

---
+++ Qesatly.Data/Enums/ContractStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace Qesatly.Data.Enums
+{
+    public enum ContractStatus
+    {
+        Active = 0,          // نشط (Active and ongoing)
+        FullyPaid = 1        // تم السداد بالكامل (Fully paid off)
+    }
+}
