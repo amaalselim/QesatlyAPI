5e836ba10a362f2063152d998b35e5bfdff4423a
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-24 23:21:42 +0300

    create dto of getclients

---
+++ Qesatly.Service/DTO/GetClientsDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Qesatly.Service.DTO
+{
+    public class GetClientsDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string productName { get; set; }
+        public string ContractStatus { get; set; }
+        public string paidInstallments { get; set; }
+    }
+}
