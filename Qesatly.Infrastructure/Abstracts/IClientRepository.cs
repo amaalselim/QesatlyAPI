4e3f2124ac4ebf93f5cc90ed3dc46187d247b7b2
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-24 23:29:54 +0300

    add parameter of search

--- Qesatly.Infrastructure/Abstracts/IClientRepository.cs
+++ Qesatly.Infrastructure/Abstracts/IClientRepository.cs
@@ -6,6 +6,6 @@ namespace Qesatly.Infrastructure.Abstracts
     public interface IClientRepository
     {
         Task<Response<string>> AddAsync(AddClientDto client);
-        Task<Response<IEnumerable<GetClientsDto>>> GetAllAsync();
+        Task<Response<IEnumerable<GetClientsDto>>> GetAllAsync(string? search = null);
     }
 }
