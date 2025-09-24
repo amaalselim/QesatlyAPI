577e44a064c7421aa0317737b3496c63f60c11c8
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-25 00:06:37 +0300

    create dashboardDto

---
+++ Qesatly.Service/DTO/dashboardDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Qesatly.Service.DTO
+{
+    public class dashboardDto
+    {
+        public int totalClients { get; set; }
+        public int totalProducts { get; set; }
+        public int activeInstallments { get; set; }
+        public int completedInstallments { get; set; }
+    }
+}
