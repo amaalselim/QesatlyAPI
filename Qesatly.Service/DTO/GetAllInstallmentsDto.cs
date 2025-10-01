f128c1afdafa8a4fbb75b981f544c65882e7f96d
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-01 18:14:10 +0300

    create dto of installments

---
+++ Qesatly.Service/DTO/GetAllInstallmentsDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Qesatly.Service.DTO
+{
+    public class GetAllInstallmentsDto
+    {
+        public int Id { get; set; }
+        public string productName { get; set; }
+        public string clientName { get; set; }
+        public decimal InstallmentValue { get; set; }
+        public DateTime DueDate { get; set; }
+        public string IsPaid { get; set; }
+
+    }
+}
