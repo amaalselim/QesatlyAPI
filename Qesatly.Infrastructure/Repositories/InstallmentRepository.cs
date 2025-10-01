834e405baacab357bfe8f7f64c4b6857a242710b
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-01 18:14:26 +0300

    create method of get all installments

---
+++ Qesatly.Infrastructure/Repositories/InstallmentRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Qesatly.Core.Bases;
+using Qesatly.Infrastructure.Abstracts;
+using Qesatly.Infrastructure.Context;
+using Qesatly.Service.DTO;
+
+namespace Qesatly.Infrastructure.Repositories
+{
+    public class InstallmentRepository : IInstallmentRepository
+    {
+        private readonly ApplicationDBContext _context;
+        private readonly ResponseHandler _responseHandler;
+
+        public InstallmentRepository(ApplicationDBContext context, ResponseHandler responseHandler)
+        {
+            _context = context;
+            _responseHandler = responseHandler;
+        }
+        public async Task<Response<IEnumerable<GetAllInstallmentsDto>>> GetAllInstallmetns()
+        {
+            var now = DateTime.Now;
+            var startOfMonth = new DateTime(now.Year, now.Month, 1);
+            var endOfMonth = startOfMonth.AddMonths(1).AddDays(-1);
+
+            var installments = await _context.installments
+               .Include(i => i.Contracts)
+                       .ThenInclude(c => c.Clients)
+               .Include(i => i.Contracts)
+                        .ThenInclude(c => c.Products)
+               .Where(i => i.DueDate >= startOfMonth && i.DueDate <= endOfMonth)
+               .ToListAsync();
+
+            var installmentDtos = installments.Select(i => new GetAllInstallmentsDto
+            {
+                Id = i.Id,
+                DueDate = i.DueDate,
+                InstallmentValue = i.Amount,
+                IsPaid = i.IsPaid ? "Paid" : "Not Paid",
+                clientName = i.Contracts.Clients.Name,
+                productName = i.Contracts.Products.Name
+            });
+            return _responseHandler.Success<IEnumerable<GetAllInstallmentsDto>>(installmentDtos);
+
+        }
+    }
+}
