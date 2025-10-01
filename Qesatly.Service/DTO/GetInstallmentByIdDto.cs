47d8007d08b3b4cd72feb58bbc75625eadc9d595
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-10-01 18:47:29 +0300

    create GetInstallmentByIdDto

---
+++ Qesatly.Service/DTO/GetInstallmentByIdDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Qesatly.Service.DTO
+{
+    public class GetInstallmentByIdDto
+    {
+        public int Id { get; set; }
+        public string clientName { get; set; }
+        public string phoneNumber { get; set; }
+        public string productName { get; set; }
+        public decimal InstallmentValue { get; set; }
+        public decimal PaidAmount { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
